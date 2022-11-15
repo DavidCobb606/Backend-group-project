93d8362e378ded3f5718a21747b057242dabd4ab
byronEsson <byronesson767@gmail.com>
2022-11-15 14:07:41 +0000

    remove commented out code

--- app.js
+++ app.js
@@ -12,28 +12,7 @@ app.post("/", (req, res, next) => {
   const {
     body: { positive, negative },
   } = req;
-  //issue was to do with a module in ipynb file so copied code into .py file
-  // and think this sorted it. console.logs in js work and can send and
-  // receive data from python
-
-  //both methods work so we just have to decide which we prefer: define options
-  // with args key and send or send directly with .send
-
-  //if nothing is coming back make sure it's the print is the last thing in the
-  //python script
-
-  //method 1
-
-  // pyshell.send({ positive: ["king", "woman"], negative: ["man"] });
-  // pyshell.on("message", (message) => res.send(message));
-
-  // // pyshell.end(function (err, code, signal) {
-  // //   if (err) throw err;
-  // //   console.log("finished");
-  // // });
-
-  //method 2
-
+ 
   let options = {
     mode: "json",
     // pythonOptions: ["-u"],
